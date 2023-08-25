e5a3181f5e9ab1b31cb5c1879c7730558f9b2a14
Richard Tanner <rtanner@alphabroder.com>
2023-08-25 15:34:16 -0400

    Add lsp reference keymap

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -68,6 +68,7 @@ keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = true }))<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
+keymap('n', '<leader>r', ":lua require('telescope.builtin').lsp_references()<cr>", opts)
 
 -- Nvimtree
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
