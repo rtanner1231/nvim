75c1f595e89d1aabc6a349a0eb616617bcc5e1db
Richard Tanner <rtanner@myron.com>
2024-04-10 21:03:29 -0400

    change null-ls to none-ls

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -50,7 +50,7 @@ return {
   "neovim/nvim-lspconfig", -- enable LSP
   "williamboman/mason.nvim", -- simple to use language server installer
   "williamboman/mason-lspconfig.nvim", -- simple to use language server installer
-  'jose-elias-alvarez/null-ls.nvim', -- LSP diagnostics and code actions
+  'nvimtools/none-ls.nvim', -- LSP diagnostics and code actions
 
   -- Telescope
   "nvim-telescope/telescope.nvim",
@@ -62,7 +62,7 @@ return {
     build = ":TSUpdate",
     commit="d94e1ad9575cc211b5726f09b28ca9454aba22fe"
   },
-  'JoosepAlviste/nvim-ts-context-commentstring',
+  --'JoosepAlviste/nvim-ts-context-commentstring',
   'HiPhish/rainbow-delimiters.nvim',
   "nvim-treesitter/playground",
 
