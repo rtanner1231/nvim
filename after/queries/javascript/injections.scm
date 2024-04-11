9c83104c04c45c56c341c09c1f922841ca3a5b17
Richard Tanner <rtanner@myron.com>
2024-04-10 21:35:25 -0400

    Remove queries

--- after/queries/javascript/injections.scm
+++
@@ -1,3 +0,0 @@
-((template_string) (#match? @sql "^`[^a-zA-z0-1]*[sS][eE][lL][eE][cC][tT].*") (#offset! @sql 0 1 0 0))
-
-((template_string) (#match? @sql "^`[^a-zA-z0-1]*[wW][iI][tT][hH].*") (#offset! @sql 0 1 0 0))
